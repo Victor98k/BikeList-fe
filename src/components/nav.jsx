9357bdd83a57a80a7c92b7b5075e02a1cd46b059
Victor98k <Victorgustav98@gmail.com>
2024-05-04 00:21:54 +0200

    Comments and post now active for users. Ant design styles asw

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,17 +1,69 @@
-// nav.js
+import React, { useState } from "react";
+import { Modal, Button, Input } from "antd";
 import Styles from "../styles/nav.module.css";
 import { IoHomeSharp } from "react-icons/io5";
 
-function Nav() {
+function Nav({ onPostCreated }) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [newPostContent, setNewPostContent] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [location, setLocation] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  const handleOk = async () => {
+    // Check if author is a valid ObjectId
+    if (!isValidObjectId(author)) {
+      console.error("Invalid author ID");
+      return;
+    }
+
+    const response = await fetch("http://localhost:8080/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title,
+        content: description,
+        imageUrl: imageUrl,
+        location: location,
+        author: author,
+      }),
+    });
+
+    if (response.ok) {
+      const newPost = await response.json(); // Define newPost with the response data
+
+      console.log("New post created successfully");
+      setIsModalVisible(false);
+      onPostCreated(newPost); // Pass the new post data to the callback function
+    } else {
+      console.error("Failed to create new post");
+    }
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  // Function to validate ObjectId
+  function isValidObjectId(id) {
+    return /^[0-9a-fA-F]{24}$/.test(id);
+  }
+
   return (
     <div className={Styles.navContainer}>
       <ul className={Styles.navLinks}>
         <h1 className={Styles.logo}>StoryStream</h1>
         <li className={Styles.links}>
-          <a className={Styles.link}> Home</a>
-        </li>
-        <li className={Styles.links}>
-          <a className={Styles.link}>Post</a>
+          <a className={Styles.link} onClick={showModal}>
+            Post
+          </a>
         </li>
         <li className={Styles.links}>
           <a className={Styles.link}>Message</a>
@@ -20,6 +72,38 @@ function Nav() {
           <a className={Styles.linkcontact}>Profile</a>
         </li>
       </ul>
+      <Modal
+        title="Create a New Post"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Input
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <Input
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <Input
+          placeholder="Image URL"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+        <Input
+          placeholder="Location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <Input
+          placeholder="Author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+      </Modal>
     </div>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,108 +1,170 @@
 import React, { useState, useEffect } from "react";
-import Styles from "../styles/home.module.css";
+import { List, Avatar, Input, Button } from "antd";
+import { Comment } from "@ant-design/compatible";
+
+import styles from "../styles/home.module.css";
 import Nav from "../components/nav";
 
 function Home() {
   const [allPosts, setAllPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [commentText, setCommentText] = useState({}); // State to hold comment texts for each post
+  const [showAllComments, setShowAllComments] = useState(false); // State to toggle showing all comments
+
+  const fetchAllPosts = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/");
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await response.json();
+      setAllPosts(data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    fetch("http://localhost:8080/")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setAllPosts(data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setError(error.message);
-        setIsLoading(false);
-      });
+    fetchAllPosts();
   }, []);
 
-  // Function to fetch comment data for a given comment ID
-  const fetchComment = async (commentId) => {
-    try {
-      const response = await fetch(
-        `http://localhost:8080/comments/${commentId}`
+  const handleCommentChange = (postId, text) => {
+    setCommentText((prev) => ({ ...prev, [postId]: text }));
+  };
+
+  // Updated handleCommentSubmit function
+  const handleCommentSubmit = async (postId, author) => {
+    const comment = commentText[postId];
+    if (!comment) {
+      console.log("No comment text found, not submitting.");
+      return; // Prevent submitting empty comments
+    }
+
+    console.log("Submitting comment:", comment); // Check the comment being submitted
+    const response = await fetch("http://localhost:8080/comments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: comment,
+        postId: postId,
+        author: author,
+      }),
+    });
+
+    if (response.ok) {
+      const newComment = await response.json();
+      console.log("Comment submitted successfully:", newComment);
+      setAllPosts(
+        allPosts.map((post) => {
+          if (post._id === postId) {
+            return { ...post, comments: [...post.comments, newComment] };
+          }
+          return post;
+        })
       );
-      if (!response.ok) {
-        throw new Error("Failed to fetch comment");
-      }
-      return await response.json();
-    } catch (error) {
-      console.error("Error fetching comment:", error);
-      return null;
+      setCommentText((prev) => ({ ...prev, [postId]: "" })); // Clear input after submission
+    } else {
+      console.error("Failed to submit comment");
     }
   };
 
-  // Function to fetch author data for a given author ID
-  const fetchAuthor = async (authorId) => {
-    try {
-      const response = await fetch(`http://localhost:8080/authors/${authorId}`);
-      if (!response.ok) {
-        throw new Error("Failed to fetch author");
-      }
-      return await response.json();
-    } catch (error) {
-      console.error("Error fetching author:", error);
-      return null;
-    }
-  };
-
-  const fetchCommentsForPosts = async () => {
-    const postsWithComments = await Promise.all(
-      allPosts.map(async (post) => {
-        const comments = await Promise.all(
-          post.comments.map(async (commentId) => {
-            const comment = await fetchComment(commentId);
-            if (comment && comment.authorId) {
-              const author = await fetchAuthor(comment.authorId);
-              return { ...comment, author };
-            } else {
-              return comment;
-            }
-          })
-        );
-        return { ...post, comments };
-      })
-    );
-    setAllPosts(postsWithComments);
-  };
   return (
-    <div className={Styles.container}>
-      <Nav />
+    <div className={styles.postContainer}>
+      <Nav onPostCreated={fetchAllPosts} />
       {isLoading && <div>Loading...</div>}
       {error && <div>Error: {error}</div>}
-      <ul className={Styles.postContainer}>
-        {allPosts.map((post) => (
-          <li className={Styles.posts} key={post.id}>
-            <h3>{post.title}</h3>
-            <img src={post.imageUrl} alt="" />
-            <ul>
-              {post.comments.map((comment, index) => (
-                <li key={index}>
-                  {comment ? (
+      <List
+        itemLayout="vertical"
+        size="large"
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 10,
+        }}
+        dataSource={allPosts}
+        renderItem={(post) => (
+          <List.Item key={post._id} className={styles.posts}>
+            <List.Item.Meta
+              avatar={<Avatar src={post.authorAvatar} />}
+              title={<a href={post.href}>{post.title}</a>}
+              description={post.description}
+            />
+            <img
+              className={styles.postImage}
+              src={post.imageUrl}
+              alt={post.title}
+            />
+            <div>{post.content}</div>
+            <div className={styles.commentSection}>
+              {post.comments && post.comments.length > 0 && (
+                <>
+                  <Comment
+                    key={post.comments[0]._id}
+                    author={post.comments[0].author}
+                    avatar={<Avatar>{post.comments[0].author[0]}</Avatar>}
+                    content={<p>{post.comments[0].text}</p>}
+                  />
+                  {post.comments.length > 1 && (
                     <>
-                      <p>{comment.text}</p>
-
-                      <p>Author: {comment.author}</p>
+                      {showAllComments ? (
+                        post.comments
+                          .slice(1)
+                          .map((comment) => (
+                            <Comment
+                              key={comment._id}
+                              author={comment.author}
+                              avatar={<Avatar>{comment.author[0]}</Avatar>}
+                              content={<p>{comment.text}</p>}
+                            />
+                          ))
+                      ) : (
+                        <div>
+                          <Button
+                            type="link"
+                            onClick={() => setShowAllComments(true)}
+                          >
+                            See all comments
+                          </Button>
+                        </div>
+                      )}
                     </>
-                  ) : (
-                    "Loading comment..."
                   )}
-                </li>
-              ))}
-            </ul>
-            <p>{post.description}</p>
-          </li>
-        ))}
-      </ul>
+                  {showAllComments && (
+                    <div>
+                      <Button
+                        type="link"
+                        onClick={() => setShowAllComments(false)}
+                      >
+                        See less comments
+                      </Button>
+                    </div>
+                  )}
+                </>
+              )}
+              <Input.TextArea
+                rows={2}
+                placeholder="Add a comment..."
+                value={commentText[post._id] || ""}
+                onChange={(e) => handleCommentChange(post._id, e.target.value)}
+              />{" "}
+              <Button
+                type="primary"
+                onClick={() =>
+                  handleCommentSubmit(post._id, "Sample Comment", "John Doe")
+                }
+              >
+                Comment this post
+              </Button>
+            </div>
+          </List.Item>
+        )}
+      />
     </div>
   );
 }
